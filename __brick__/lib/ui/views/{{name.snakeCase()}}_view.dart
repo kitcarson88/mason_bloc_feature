c8696e0f2d8895a5f7a9e95159d3e487636f2725
Simone Colazzo <s.colazzo@almaviva.it>
2023-05-30 16:11:22 +0200

    Fixes

--- __brick__/lib/ui/views/{{name.snakeCase()}}_view.dart
+++ __brick__/lib/ui/views/{{name.snakeCase()}}_view.dart
@@ -1,4 +1,4 @@
-import 'package:{{app_name}}/bloc/cubit/{{name.snakeCase()}}_cubit.dart';
+import 'package:{{app_name}}/bloc/{{name.snakeCase()}}/{{name.snakeCase()}}_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
